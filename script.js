c3077718337d310360eca0a8602020ca256cfb5f
Ilse Johana Garcia Gonzalez <ilsesitakxtty@.gmail.com>
2025-10-28 18:16:50 -0600

    CalculadoraKxtty con js.

--- script.js
+++ script.js
@@ -1,20 +1,78 @@
-const display = document.getElementById("display");
-const buttons = document.querySelectorAll(".btn");
+const buttons = document.querySelectorAll('.btn');
+const display = document.querySelector('#display');
+display.value = '';
 
-buttons.forEach(btn => {
-  btn.addEventListener("click", () => {
-    const value = btn.textContent;
+class Calculator {
+  constructor() {
+    this.valor1 = null;
+    this.valor2 = null;
+    this.operation = null;
+  }
 
-    if (value === "C") {
-      display.value = "";
-    } else if (value === "=") {
-      try {
-        display.value = eval(display.value);
-      } catch {
-        display.value = "Error";
-      }
-    } else {
+  setValueOne(value1) {
+    this.valor1 = value1;
+  }
+
+  setValueTwo(value2) {
+    this.valor2 = value2;
+  }
+
+  setOperation(op) {
+    this.operation = op;
+  }
+
+  sum(a, b) { return a + b; }
+  rest(a, b) { return a - b; }
+  divide(a, b) { return a / b; }
+  multiplication(a, b) { return a * b; }
+
+  calculate() {
+    if (this.operation === '+') return this.sum(this.valor1, this.valor2);
+    if (this.operation === '-') return this.rest(this.valor1, this.valor2);
+    if (this.operation === '*') return this.multiplication(this.valor1, this.valor2);
+    if (this.operation === '/') return this.divide(this.valor1, this.valor2);
+  }
+
+  clear() {
+    this.valor1 = null;
+    this.valor2 = null;
+    this.operation = null;
+  }
+}
+
+const calculator = new Calculator();
+
+buttons.forEach(button => {
+  button.addEventListener('click', (e) => {
+    const value = e.target.innerText;
+
+    if (value === 'C') {
+      display.value = '';
+      calculator.clear();
+      return;
+    }
+
+
+    if (!isNaN(value) || value === '.') {
       display.value += value;
+      return;
+    }
+
+    if (value === '+' || value === '-' || value === 'x' || value === '÷') {
+      calculator.setValueOne(Number(display.value));
+
+      const op = value === 'x' ? '*' : value === '÷' ? '/' : value;
+      calculator.setOperation(op);
+      display.value = '';
+      return;
+    }
+
+
+    if (value === '=') {
+      calculator.setValueTwo(Number(display.value));
+      const result = calculator.calculate();
+      display.value = result;
+      calculator.clear();
     }
   });
 });
